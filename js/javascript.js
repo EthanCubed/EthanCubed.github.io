b84caa6ea957d01cd81f8c9931163485ecb828bf
EthanCubed <evandrevalas@gmail.com>
2022-11-24 10:33:17 -0500

    jsBroke Attempting fix

--- js/javascript.js
+++ js/javascript.js
@@ -1,27 +1,213 @@
 const pinCheckArray = [true, true, true, true, true, true, true, true, true, true];
 //true for up, false for down
 
-const pins = 
-    [["onePin", true],
-    ["twoPin", true],
-    ["threePin", true],
-    ["fourPin", true],
-    ["fivePin", true],
-    ["sixPin", true],
-    ["sevenPin", true],
-    ["eightPin", true],
-    ["ninePin", true],
-    ["tenPin", true]
-    ];
+currentFrame = 1;
+currentShot = 1;
 
+const onePin = {
+    name: "onePin",
+    value: true,
+    number: 1,
+    toggle: function() {
+        this.value = !this.value;
+    }
+};
 
+const twoPin = {
+    name: "twoPin",
+    value: true,
+    number: 2,
+    toggle: function() {
+        this.value = !this.value;
+    }
+};
+
+const threePin = {
+    name: "threePin",
+    value: true,
+    number: 3,
+    toggle: function() {
+        this.value = !this.value;
+    }
+};
+
+const fourPin = {
+    name: "fourPin",
+    value: true,
+    number: 4,
+    toggle: function() {
+        this.value = !this.value;
+    }
+};
+
+const fivePin = {
+    name: "fivePin",
+    value: true,
+    number: 5,
+    toggle: function() {
+        this.value = !this.value;
+    }
+};
+
+const sixPin = {
+    name: "sixPin",
+    value: true,
+    number: 6,
+    toggle: function() {
+        this.value = !this.value;
+    }
+};
+
+const sevenPin = {
+    name: "sevenPin",
+    value: true,
+    number: 7,
+    toggle: function() {
+        this.value = !this.value;
+    }
+};
+
+const eightPin = {
+    name: "eightPin",
+    value: true,
+    number: 8,
+    toggle: function() {
+        this.value = !this.value;
+    }
+};
+
+const ninePin = {
+    name: "ninePin",
+    value: true,
+    number: 9,
+    toggle: function() {
+        this.value = !this.value;
+    }
+};
+
+const tenPin = {
+    name: "tenPin",
+    value: true,
+    number: 10,
+    toggle: function() {
+        this.value = !this.value;
+    }
+};
+
+const firstFrame = {
+    name: "F1",
+    scoreName: "FS1",
+    firstShotScore: 0,
+    seocndShotScore: 0,
+    score: calcScore(this, secondFrame, thirdFrame)
+};
+
+const secondFrame = {
+    name: "F2",
+    scoreName: "FS2",
+    firstShotScore: 0,
+    seocndShotScore: 0,
+    score: calcScore(this, thirdFrame, fourthFrame)
+};
+
+const thirdFrame = {
+    name: "F3",
+    scoreName: "FS3",
+    firstShotScore: 0,
+    seocndShotScore: 0,
+    score: calcScore(this, fourthFrame, fifthFrame)
+};
+
+const fourthFrame = {
+    name: "F4",
+    scoreName: "FS4",
+    firstShotScore: 0,
+    seocndShotScore: 0,
+    score: calcScore(this, fifthFrame, sixthFrame)
+};
+
+const fifthFrame = {
+    name: "F5",
+    scoreName: "FS5",
+    firstShotScore: 0,
+    seocndShotScore: 0,
+    score: calcScore(this, sixthFrame, seventhFrame)
+};
+
+const sixthFrame = {
+    name: "F6",
+    scoreName: "FS6",
+    firstShotScore: 0,
+    seocndShotScore: 0,
+    score: calcScore(this, seventhFrame, eighthFrame)
+};
+
+const seventhFrame = {
+    name: "F7",
+    scoreName: "FS7",
+    firstShotScore: 0,
+    seocndShotScore: 0,
+    score: calcScore(this, eighthFrame, ninthFrame)
+};
+
+const eighthFrame = {
+    name: "F8",
+    scoreName: "FS8",
+    firstShotScore: 0,
+    seocndShotScore: 0,
+    score: calcScore(this, ninthFrame, tenthFrame)
+};
+
+const ninthFrame = {
+    name: "F9",
+    scoreName: "FS9",
+    firstShotScore: 0,
+    seocndShotScore: 0,
+    score: calcScore(this, tenthFrame, sixthFrame)
+};
+
+const tenthFrame = {
+    name: "F10",
+    scoreName: "FS10",
+    firstShotScore: 0,
+    seocndShotScore: 0,
+    thirdShotScore: 0,
+    score: function() {
+        this.firstShotScore + 
+        this.seocndShotScore +
+        this.thirdShotScore;
+    },
+    thirdShotAvailable: function(){
+        if((this.firstShotScore + this.seocndShotScore) >= 10){
+            return true;
+        }
+    }
+};
+
+function calcScore(x, y, z){
+    score = 0;
+    if(x.firstShotScore == 10){
+        score += (x.firstShotScore + y.firstShotScore);
+
+        if(y.firstShotScore == 10){
+            score += z.firstShotScore;
+        }else{
+            score += y.seocndShotScore;
+        }
+    }
+    return score;
+}
+
+const pins = [onePin, twoPin, threePin, fourPin, fivePin, sixPin, sevenPin, eightPin, ninePin, tenPin];
+
+const frmes = [firstFrame, secondFrame, thirdFrame, fourthFrame, fifthFrame, sixthFrame, seventhFrame, eighthFrame, ninthFrame, tenthFrame]
 
 function onMouseOverPin(number){
-    x = document.getElementById(pins[number-1][0]);
+    x = document.getElementById(pins[number - 1].name.valueOf());
 
     backgroundColor = x.style.backgroundColor;
     
-    if(pinCheckArray[number - 1].valueOf()){
+    if(pins[number - 1].value.valueOf()){
         x.style.color = "white";
         x.style.backgroundColor = "darkgrey";
     }else{
@@ -35,7 +221,7 @@ function onMouseOutPin(name, number){
 
     backgroundColor = x.style.backgroundColor;
 
-    if(pinCheckArray[number - 1].valueOf()){
+    if(pins[number - 1].value.valueOf()){
         x.style.color = "black";
         x.style.backgroundColor = "white";
     }else{
@@ -45,9 +231,9 @@ function onMouseOutPin(name, number){
 }
 
 function pinClick(name, number){
-    x = document.getElementById(name);
+    x = document.getElementById(pins[number - 1].name.valueOf());
 
-    if(pinCheckArray[number - 1].valueOf()){
+    if(pins[number - 1].value.valueOf()){
         x.style.color = "white";
         x.style.backgroundColor = "darkgrey";
     }else{
@@ -55,27 +241,55 @@ function pinClick(name, number){
         x.style.backgroundColor = "white";
     }
 
-    pinCheckArray[number - 1] = !pinCheckArray[number - 1];
+    pins[number - 1].toggle();  
     
-    updateScore(0);
+    updateScore();
     
+}
+
+function getCount(){
+    count = 0;
+
+    for(i = 0; i < pins.length; i++){
+        if(!pins[i].value.valueOf()){
+            count++;
+        }
+    }
+    return count;
+}
+
+function strikeSpare(){
+
+    for(i = 0; i < pins.length; i++){
+        pins[i].value = false;
+
+        document.getElementById(pins[i].name.valueOf()).style.color = "white";
+        document.getElementById(pins[i].name.valueOf()).style.backgroundColor = "darkgrey";
+    }
+
+    updateScore();
 }
 
 function updateScore(){
     score = 0
-    for(i = 0; i < pinCheckArray.length; i++){
-        if(!pinCheckArray[i].valueOf()){
+    for(i = 0; i < pins.length; i++){
+        if(!pins[i].value.valueOf()){
             score++;
         }
     }
 
     if(score == 10){
         score = 'X';
+    }else if(score == 0){
+        score = '-';
     }
 
-    document.getElementById("F1_S1").innerHTML = score.valueOf();
+    document.getElementById("F1").innerHTML = score.valueOf();
 }
 
 function progressNext(){
+    document.getElementById("F10").innerHTML = getCount();
+    if(currentShot == 1){
 
+    }
 }
